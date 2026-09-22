47cb91db1c1a22a3c43f1ac21c2b28392fdb69cb
Néstor <n.antontolentino@colegiovirgendeeuropa.edu.es>
2026-09-22 11:42:37 +0200

    ForLoop4 y Forloop3

--- src/basics/ForLooop4.java
+++ src/basics/ForLooop4.java
@@ -4,7 +4,11 @@ public class ForLooop4 {
 
 		public static void main(String[] args) {
 			
+			int sum = 1 +2+3+4+5;
+			System.out.println(sum);
 			// añade nums de 1 a 1000
+			sum = 0;
+			
 			for (int i = 0; i < 20; i++) {
 				
 			}
